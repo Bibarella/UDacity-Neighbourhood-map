7192653c3f5f0077196da1b8736d34a29740a948
Bianca Glasner <neotrinity1501@yahoo.de>
2019-02-13 17:53:10 +0100

    added function to get script from index.html

--- src/App.js
+++ src/App.js
@@ -21,4 +21,15 @@ class App extends Component {
   }
 }
 
+/* loads script from index.html */ 
+function loadScript(url) {
+	let index = window.document.getElementsByTagName("script")[0]
+	let script = window.document.createElement("script")
+	script.src = url
+	script.async = true
+	script.defer = true
+	index.parentNode.insertBefore(script, index)
+	
+}
+
 export default App;
